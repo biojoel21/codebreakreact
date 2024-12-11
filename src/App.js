500aa19ab9a785936f1a8bf886802e7d0d41cdac
biojoel21 <eebc_5@hotmail.com>
2024-12-11 11:55:25 -0400

    terminar capt 7

--- src/App.js
+++ src/App.js
@@ -2,28 +2,34 @@ import './index.css';
 import Employee from './components/Employee';
 import { useState } from 'react';
 
-function App() {  
+function App() {
   const [role, setRole] = useState('dev');
   const showEmployee = true;
   return (
-    <div className="App bg-red-300">
-      { showEmployee ?
+    <div className="App">
+      {showEmployee ?
         <>
-        <input 
-          type="text" 
-          onChange={(e) => {
-            console.log(e.target.value)
-            setRole(e.target.value);
-          }}        
-        />
-        <Employee name="Caleb" role="Intern"/>
-        <Employee name="Carlos" role="Intern" />
-        <Employee name="Luis" role="Employee" />
-        <Employee name="Nestor" role={role} />
-        <Employee />
+          <input
+            type="text"
+            onChange={(e) => {
+              console.log(e.target.value)
+              setRole(e.target.value);
+            }}
+          />
+          <div className='flex flex-wrap justify-center'>
+            <Employee name="Caleb" role="Intern"  img="https://images.pexels.com/photos/29096929/pexels-photo-29096929/free-photo-of-majestic-neptune-statue-at-trevi-fountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+            <Employee name="Carlos" role="Intern" img="https://images.pexels.com/photos/29096929/pexels-photo-29096929/free-photo-of-majestic-neptune-statue-at-trevi-fountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"   />
+            <Employee name="Luis" role="Employee" img="https://images.pexels.com/photos/29096929/pexels-photo-29096929/free-photo-of-majestic-neptune-statue-at-trevi-fountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+            <Employee name="Caleb" role="Intern"  img="https://images.pexels.com/photos/29096929/pexels-photo-29096929/free-photo-of-majestic-neptune-statue-at-trevi-fountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+            <Employee name="Carlos" role="Intern" img="https://images.pexels.com/photos/29096929/pexels-photo-29096929/free-photo-of-majestic-neptune-statue-at-trevi-fountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+            <Employee name="Luis" role="Employee" img="https://images.pexels.com/photos/29096929/pexels-photo-29096929/free-photo-of-majestic-neptune-statue-at-trevi-fountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+            <Employee name="Caleb" role="Intern"  img="https://images.pexels.com/photos/29096929/pexels-photo-29096929/free-photo-of-majestic-neptune-statue-at-trevi-fountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+            <Employee name="Carlos" role="Intern" img="https://images.pexels.com/photos/29096929/pexels-photo-29096929/free-photo-of-majestic-neptune-statue-at-trevi-fountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+            <Employee name="Luis" role="Employee" img="https://images.pexels.com/photos/29096929/pexels-photo-29096929/free-photo-of-majestic-neptune-statue-at-trevi-fountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+          </div>
         </>
-        : <p>You cannot see the employees</p>    
-      } 
+        : <p>You cannot see the employees</p>
+      }
     </div>
   );
 }
