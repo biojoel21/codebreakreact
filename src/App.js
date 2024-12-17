01172b074ebf6416d3827ac2db902bd547a4a2d7
biojoel21 <eebc_5@hotmail.com>
2024-12-17 17:55:19 -0400

    terminar capt 8

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './index.css';
 import Employee from './components/Employee';
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [role, setRole] = useState('dev');
+  const [employees, setEmployees] = useState([
+    {
+      name: 'Caleb',
+      role: 'Intern',
+      img: 'https://images.pexels.com/photos/29096929/pexels-photo-29096929/free-photo-of-majestic-neptune-statue-at-trevi-fountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+      name: 'Carlos',
+      role: 'Intern',
+      img: 'https://images.pexels.com/photos/29096929/pexels-photo-29096929/free-photo-of-majestic-neptune-statue-at-trevi-fountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+      name: 'Luis',
+      role: 'Employee',
+      img: 'https://images.pexels.com/photos/29096929/pexels-photo-29096929/free-photo-of-majestic-neptune-statue-at-trevi-fountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+  ]);
   const showEmployee = true;
   return (
     <div className="App">
@@ -17,15 +35,16 @@ function App() {
             }}
           />
           <div className='flex flex-wrap justify-center'>
-            <Employee name="Caleb" role="Intern"  img="https://images.pexels.com/photos/29096929/pexels-photo-29096929/free-photo-of-majestic-neptune-statue-at-trevi-fountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-            <Employee name="Carlos" role="Intern" img="https://images.pexels.com/photos/29096929/pexels-photo-29096929/free-photo-of-majestic-neptune-statue-at-trevi-fountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"   />
-            <Employee name="Luis" role="Employee" img="https://images.pexels.com/photos/29096929/pexels-photo-29096929/free-photo-of-majestic-neptune-statue-at-trevi-fountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-            <Employee name="Caleb" role="Intern"  img="https://images.pexels.com/photos/29096929/pexels-photo-29096929/free-photo-of-majestic-neptune-statue-at-trevi-fountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-            <Employee name="Carlos" role="Intern" img="https://images.pexels.com/photos/29096929/pexels-photo-29096929/free-photo-of-majestic-neptune-statue-at-trevi-fountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-            <Employee name="Luis" role="Employee" img="https://images.pexels.com/photos/29096929/pexels-photo-29096929/free-photo-of-majestic-neptune-statue-at-trevi-fountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-            <Employee name="Caleb" role="Intern"  img="https://images.pexels.com/photos/29096929/pexels-photo-29096929/free-photo-of-majestic-neptune-statue-at-trevi-fountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-            <Employee name="Carlos" role="Intern" img="https://images.pexels.com/photos/29096929/pexels-photo-29096929/free-photo-of-majestic-neptune-statue-at-trevi-fountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-            <Employee name="Luis" role="Employee" img="https://images.pexels.com/photos/29096929/pexels-photo-29096929/free-photo-of-majestic-neptune-statue-at-trevi-fountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+              { employees.map((employee) => {
+                 return(
+                    <Employee                
+                      key={uuidv4()}      
+                      name={employee.name}
+                      role={employee.role}
+                      img={employee.img}
+                    />
+                 );
+              })}
           </div>
         </>
         : <p>You cannot see the employees</p>
