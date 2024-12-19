fc16c6e4ab579ce9ea1277f1abbd25b0805d5471
biojoel21 <eebc_5@hotmail.com>
2024-12-19 11:52:16 -0400

    terminar capt 12

--- src/App.js
+++ src/App.js
@@ -7,21 +7,35 @@ function App() {
   const [role, setRole] = useState('dev');
   const [employees, setEmployees] = useState([
     {
+      id: uuidv4(),
       name: 'Caleb',
       role: 'Intern',
       img: 'https://images.pexels.com/photos/29096929/pexels-photo-29096929/free-photo-of-majestic-neptune-statue-at-trevi-fountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
     },
     {
+      id: uuidv4(),
       name: 'Carlos',
       role: 'Intern',
       img: 'https://images.pexels.com/photos/29096929/pexels-photo-29096929/free-photo-of-majestic-neptune-statue-at-trevi-fountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
     },
     {
+      id: uuidv4(),
       name: 'Luis',
       role: 'Employee',
       img: 'https://images.pexels.com/photos/29096929/pexels-photo-29096929/free-photo-of-majestic-neptune-statue-at-trevi-fountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
     },
   ]);
+
+  function updateEmployee(id, newName, newRole){
+      const updateEmployee = employees.map((employee) => {
+          if(id === employee.id){
+              return {...employee, name: newName, role: newRole};
+          }
+          return employee;
+      } );
+      setEmployees(updateEmployee);
+  }
+
   const showEmployee = true;
   return (
     <div className="App">
@@ -38,10 +52,12 @@ function App() {
               { employees.map((employee) => {
                  return(
                     <Employee                
-                      key={uuidv4()}      
+                      key={employee.id}      
+                      id={employee.id}
                       name={employee.name}
                       role={employee.role}
                       img={employee.img}
+                      updateEmployee={updateEmployee}
                     />
                  );
               })}
--- src/components/EditEmployee.js
+++ src/components/EditEmployee.js
@@ -13,7 +13,7 @@ function EditEmployee(props) {
 
     return (
         <>
-            <button onClick={handleShow} classNameName="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Update</button>
+            <button onClick={handleShow} className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Update</button>
 
             <Modal
                 show={show}
@@ -25,7 +25,12 @@ function EditEmployee(props) {
                     <Modal.Title>Edit Employee</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <form id='editmodal' className="w-full max-w-sm">
+                    <form 
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            props.updateEmployee(props.id,name,role);                            
+                        }}
+                        id='editmodal' className="w-full max-w-sm">
                         <div className="md:flex md:items-center mb-6">
                             <div className="md:w-1/3">
                                 <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="name">
@@ -62,7 +67,7 @@ function EditEmployee(props) {
                     </form>
                 </Modal.Body>
                 <Modal.Footer>
-                    <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-gray-500 py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={handleClose}>
+                    <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-gray-500 py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={handleClose}>
                         Close
                     </button>
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" form="editmodal">Update</button>
