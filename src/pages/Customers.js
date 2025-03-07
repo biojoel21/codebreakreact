7f68ee35ed8d5bcfd5ea45ae0723ba8078e2b049
biojoel21 <eebc_5@hotmail.com>
2025-03-07 16:00:24 -0400

    terminar capt 36

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { baseUrl } from '../shared';
+import AddCustomer from '../components/AddCustomer';
 
 export default function Customers() {
     const [customers, setCustomers] = useState();
@@ -12,6 +13,26 @@ export default function Customers() {
                 setCustomers(data.customers);
             });
     }, []);
+    function newCustomer(name, industry){
+        const data = {name: name, industry: industry};
+        const url = baseUrl + 'api/customers';
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then((response) => {
+            if(!response.ok) {
+                throw new Error('Something went wrong');
+            }
+            return response.json();
+        }).then((data) => {
+            //
+        }).catch((e) => {
+            console.log(e);
+        });
+    }
     return (
         <>
             <h1>Here are our customers:</h1>
@@ -24,6 +45,7 @@ export default function Customers() {
                     );
                 }) : null}
             </ul>
+            <AddCustomer newCustomer={newCustomer} />
         </>
     );
 }
